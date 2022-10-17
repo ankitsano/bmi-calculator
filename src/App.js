e9107f93e11ed5bea5fd52e1da90124554d8a083
ankit <ankitsanodiya789@gmail.com>
2022-10-17 09:28:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
+import Amazon from "./components/amazon";
+import Navbar from "./components/navbar";
+import Cart from "./components/cart";
 
-import './index.css'
+const App = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
 
-function App() {
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
 
-  // state
-  const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
 
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
 
-
-  let calcBmi = (event) => {
-    //prevent submitting
-    event.preventDefault()
-
-    if (weight === 0 || height === 0) {
-      alert('Please enter a valid weight and height')
-    } else {
-      let bmi = (weight / (height * height) * 703)
-      setBmi(bmi.toFixed(1))
-
-      // Logic for message
-
-      if (bmi < 25) {
-        setMessage('You are underweight')
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage('You are a healthy weight')
-      } else {
-        setMessage('You are overweight')
-      }
-    }
-  }
-
-
-  let reload = () => {
-    window.location.reload()
-  }
 
   return (
-    <div className="app">
-      <div className='container'>
-        <h2 className='center'>BMI Calculator</h2>
-        <form onSubmit={calcBmi}>
-          <div>
-            <label>Weight (lbs)</label>
-            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
-          </div>
-          <div>
-            <label>Height (in)</label>
-            <input value={height} onChange={(event) => setHeight(event.target.value)} />
-          </div>
-          <div>
-            <button className='btn' type='submit'>Submit</button>
-            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
-          </div>
-        </form>
-
-        <div className='center'>
-          <h3>Your BMI is: {bmi}</h3>
-          <p>{message}</p>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
+    </React.Fragment>
   );
-}
+};
 
 export default App;
-
-const myStudents=(<ul>
-  <li></li>
-  <li></li>
-</ul>)
